6ea63855946b622e22391c86c510d6eba0458fdf
Chinaka-manuel <chinakamanuel@gmail.com>
2024-05-04 16:33:55 +0100

    digismath registration landing page

---
+++ jquery.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    $("#menuslid").click(function(){
+        $("#navi").fadeToggle(1500)
+    }
+    )
+
+  
+  });
---
+++ script.js
@@ -0,0 +1,36 @@
+const mediaQuery = window.matchMedia("(max-width:700px)");
+
+
+const clicks=()=>{
+    
+    const clickOut= window;
+    let navbar=document.getElementById("navi");
+    
+    if(navbar.style.display.endsWith("block")||mediaQuery){
+       return clickOut.addEventListener("scroll", ()=>{
+            navbar.style.display="none"
+        })
+    } else{
+        navbar.style.display="block"
+    };
+
+}
+
+clicks();
+
+
+// const navslide= ()=>{
+//     let btn = document.getElementById("menuslid");
+//     let navi = document.getElementById("navi");
+//      btn.addEventListener("click", ()=>{
+//         if (navi.style.display.endsWith("block") && mediaQuery){
+//             navi.style.display="none"
+//         } else{
+//             navi.style.display="block"
+//         }
+//      })
+// }
+
+// navslide();
+
+
